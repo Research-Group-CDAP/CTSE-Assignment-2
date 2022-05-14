70e448bac31c3cec52eb44f97ea321e9bf79e03f
Lasal Hettiarachchi <it19132310@my.sliit.lk>
2022-05-14 12:29:24 +0530

    implementing getUsers and getUsers List endpoints

--- src/user_service/controller/user.controller.js
+++ src/user_service/controller/user.controller.js
@@ -1,19 +1,19 @@
 import User from '../models/user.model.js'
 
-const login = async(req,res) =>{
-    res.status(200).send({"msg":"tokencalled"})
+const login = async (req, res) => {
+    res.status(200).send({ "msg": "tokencalled" })
 }
 
-const register = async(req,res) =>{
-    const { first_name, last_name, email, mobileNumber,address} = req.body;
+const register = async (req, res) => {
+    const { first_name, last_name, email, mobileNumber, address } = req.body;
     try {
         //See if user Exist
         let user = await User.findOne({ email });
-    
+
         if (user) {
-          return res
-            .status(400)
-            .json({ errors: [{ msg: "User already exist" }] });
+            return res
+                .status(400)
+                .json({ errors: [{ msg: "User already exist" }] });
         }
 
         var date = new Date()
@@ -21,31 +21,29 @@ const register = async(req,res) =>{
         const updatedAt = date;
         //create a user instance
         user = new User({
-            first_name,last_name,email,mobileNumber,address,createdAt,updatedAt
+            first_name, last_name, email, mobileNumber, address, createdAt, updatedAt
         });
         //save user to the database
         await user.save().then((response) => {
-          res.json(response);
+            res.json(response);
         });
-      } catch (err) {
+    } catch (err) {
         //Something wrong with the server
         console.error(err.message);
         return res.status(500).send("Server Error");
-      }
+    }
 }
 
-const updateUser = async(req,res) =>{
+const updateUser = async (req, res) => {
     try {
         const user = await User.findById(req.params.id);
-        
-    
         if (user != null) {
             User.findByIdAndUpdate(req.params.id).then(async (userProfile) => {
                 if (req.body.first_name) {
-                  userProfile.first_name = req.body.first_name;
+                    userProfile.first_name = req.body.first_name;
                 }
                 if (req.body.last_name) {
-                  userProfile.last_name = req.body.last_name;
+                    userProfile.last_name = req.body.last_name;
                 }
                 if (req.body.email) {
                     userProfile.email = req.body.email;
@@ -59,26 +57,48 @@ const updateUser = async(req,res) =>{
                 var date = new Date()
                 userProfile.updatedAt = date
                 userProfile
-                  .save()
-                  .then((response) => res.json(response))
-                  .catch((err) => res.status(400).json("Error: " + err));
-              });
-            }
-                
-      } catch (err) {
+                    .save()
+                    .then((response) => res.json(response))
+                    .catch((err) => res.status(400).json("Error: " + err));
+            });
+        }
+
+    } catch (err) {
         //Something wrong with the server
         console.error(err.message);
         return res.status(500).send("Server Error");
-      }
+    }
 }
-const deleteUser = async(req,res) =>{
-    res.status(200).send({"msg":"tokencalled"})
+const deleteUser = async (req, res) => {
+    try {
+        await User.findByIdAndDelete(req.params.id)
+            .then(() => {
+                res.json("Employee Deleted");
+            })
+            .catch((err) => res.status(400).json("Error: " + err));
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send("Server Error");
+    }
+
 }
-const getUserDetailsbiID = async(req,res) =>{
-    res.status(200).send({"msg":"tokencalled"})
+const getUserDetailsbyID = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+        res.json(user);
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send("Server Error");
+    }
 }
-const getUserList = async(req,res) =>{
-    res.status(200).send({"msg":"tokencalled"})
+const getUserList = async (req, res) => {
+    try {
+        const userList = await User.find()
+        res.json(userList);
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send("Server Error");
+    }
 }
 
-export {login,register,updateUser,deleteUser,getUserDetailsbiID,getUserList}
+export { login, register, updateUser, deleteUser, getUserDetailsbyID, getUserList }
--- src/user_service/routes/user.routes.js
+++ src/user_service/routes/user.routes.js
@@ -1,13 +1,13 @@
 import express from "express"
-import {login,register,updateUser,deleteUser,getUserDetailsbiID,getUserList} from "../controller/user.controller.js"
+import {login,register,updateUser,deleteUser,getUserDetailsbyID,getUserList} from "../controller/user.controller.js"
 const userRoutes = express.Router();
 
 
 userRoutes.get("/login",  login);
 userRoutes.post("/register",  register);
 userRoutes.put("/update/:id",  updateUser);
-userRoutes.delete("/delete",  deleteUser);
-userRoutes.get("/:id",  getUserDetailsbiID);
+userRoutes.delete("/delete/:id",  deleteUser);
+userRoutes.get("/:id",  getUserDetailsbyID);
 userRoutes.get("/",  getUserList);
 
 export default userRoutes;
