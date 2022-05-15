016176fe87648fb50c99a30689de8f2b06c358f7
Dilmi <dnimasha971@gmail.com>
2022-05-15 15:45:18 +0530

    Payment service Implementation

---
+++ src/delivery_service/src/main/java/com/ctse/domain/TransactionHeader.java
@@ -0,0 +1,15 @@
+package com.ctse.domain;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(value = "transactionHeaders")
+public class TransactionHeader {
+
+	@Id
+	private String id;
+	
+	private String ;
+	
+	private String 
+}
