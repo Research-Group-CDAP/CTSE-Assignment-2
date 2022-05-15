535c17082534930c6be9aca3349af7d884a96319
senuravihanjayadeva <senurajayadeva@gmail.com>
2022-05-15 23:30:37 +0530

    Updated

--- src/cart_service/src/main/java/com/ctse/cartservice/controller/HomeController.java
+++ src/cart_service/src/main/java/com/ctse/cartservice/controller/HomeController.java
@@ -12,7 +12,7 @@ public class HomeController {
 
     @GetMapping("/")
     public ResponseEntity<?> getHomePage(){
-        return new ResponseEntity<>("Cart Microservice Running - Updated", HttpStatus.OK);
+        return new ResponseEntity<>("Cart Microservice Running", HttpStatus.OK);
     }
 
 }
