53b157aa507941ff5eb83b7a8ade4194b34d3616
rusiruavb <rusiruavb98@gmail.com>
2022-05-15 11:31:21 +0000

    Implement get order by id

--- src/order_service/controllers/order_controller.go
+++ src/order_service/controllers/order_controller.go
@@ -33,11 +33,11 @@ func CreateOrder(c *fiber.Ctx) error {
 	}
 
 	newOrder := models.Order{
-		OrderId: primitive.NewObjectID(),
-		OrderDate: time.Now().String(),
+		OrderId:          primitive.NewObjectID(),
+		OrderDate:        time.Now().String(),
 		OrderDiscription: order.OrderDiscription,
-		OrderFee: order.OrderFee,
-		Products: order.Products,
+		OrderFee:         order.OrderFee,
+		Products:         order.Products,
 	}
 
 	_, err := orderCollection.InsertOne(ctx, newOrder)
@@ -78,12 +78,37 @@ func GetOrders(c *fiber.Ctx) error {
 	return nil
 }
 
+func GetOrderById(c *fiber.Ctx) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+	orderId := c.Params("orderId")
+	fmt.Println(orderId)
+	defer cancel()
+
+	objId, _ := primitive.ObjectIDFromHex(orderId)
+
+	var order models.Order
+	if err := orderCollection.FindOne(ctx, bson.M{"orderid": objId}).Decode(&order); err != nil {
+		handlers.SendErrorResponse(c, &fiber.Map{"message": err.Error()})
+	}
+
+	for i, prod := range order.Products {
+		var product = getProductInfo(prod.ProductId)
+		order.Products[i].Info.ProductTitle = product.ProductTitle
+		order.Products[i].Info.ImageURL = product.ImageURL
+		order.Products[i].Info.Price = product.Price
+		order.Products[i].Info.CategoryId = product.CategoryId
+	}
+
+	handlers.SendSuccessResponse(c, &fiber.Map{"data": order})
+	return nil
+}
+
 func getProductInfo(productId primitive.ObjectID) models.OrderProduct {
 	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
 	defer cancel()
 
 	var product models.OrderProduct
-	err := productCollection.FindOne(ctx, bson.M{"_id": productId }).Decode(&product)
+	err := productCollection.FindOne(ctx, bson.M{"_id": productId}).Decode(&product)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
